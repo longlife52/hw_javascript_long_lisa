deb25c18d7e16178d7fee1666d23997669ac7f22
Lisa Long <garrisonridge@gmail.com>
2017-01-29 22:34:52 -0500

    Add js presidents43-40

--- js/main.js
+++ js/main.js
@@ -5,3 +5,35 @@ var president44 = {
     party: 'Democrat',
     yearsOfPresidency: '2009-2017'
 };
+
+var president43 = {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: 8,
+    party: 'Republican',
+    yearsOfPresidency: '2001-2009'
+};
+
+var president42 = {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: 8,
+    party: 'Democrat',
+    yearsOfPresidency: '1993-2001'
+};
+
+var president41 = {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: 4,
+    party: 'Republican',
+    yearsOfPresidency: '1989-1993'
+};
+
+var president40 = {
+    firstName: 'Ronald',
+    lastName: 'Reagan',
+    termLength: 8,
+    party: 'Republican',
+    yearsOfPresidency: '1981-1989'
+};

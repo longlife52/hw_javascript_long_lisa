112192f8c268eb5eae047a399d8cb571d20d3392
Lisa Long <garrisonridge@gmail.com>
2017-01-30 22:01:22 -0500

    Add function expression: numba

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ var message = howdy('Lisa ', 'her best.');
 console.log(message);
 
 howdy();
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    } if (string.length > 7) {
+        console.log("I'm sorry, but that's too many to count");
+    } if (string.length === 7) {
+        console.log('7, what a perfect choice!');
+    }
+};
+
+numba('7');
+numba('seventy');
+numba('university');
